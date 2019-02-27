6aab81050cab4d0af701b30d6a555ab8e8b1e07b
Tautvydas Versockas <tautvydas.versockas@gmail.com>
2019-02-27 23:03:34 +0200

    Baseline model.

---
+++ DogsVsCatsClassification.py
@@ -0,0 +1,136 @@
+import os
+import shutil
+import matplotlib.pyplot as plt
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
+base_data_dir = '/content/drive/My Drive/Keras/kaggle_original_data/train'
+base_dir = '/content/drive/My Drive/Keras/dogs_vs_cats'
+model_path = '/content/drive/My Drive/Keras/dogs_vs_cats/model_1.h5'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+test_cats_dir = os.path.join(test_dir, 'cats')
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+def copy_files() :
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(train_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(validation_cats_dir, fname)
+        shutil.copyfile(src, dst)
+  
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(test_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(train_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(validation_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+  
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(base_data_dir, fname)
+        dst = os.path.join(test_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+    
+    print(len(os.listdir(train_cats_dir)))
+    print(len(os.listdir(test_cats_dir)))
+    print(len(os.listdir(validation_cats_dir)))
+    print(len(os.listdir(train_dogs_dir)))
+    print(len(os.listdir(test_dogs_dir)))
+    print(len(os.listdir(validation_dogs_dir)))
+
+def display_results(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validatio acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validatio loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+
+def build_model():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150,150,3)))
+    model.add(layers.MaxPooling2D((2,2 )))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2,2 )))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2,2 )))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2,2 )))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizers.RMSprop(lr=1e-4),
+                  metrics=['acc'])
+    return model
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+model = build_model()
+print(model.summary())
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+model.save(model_path)
+display_results(history)
